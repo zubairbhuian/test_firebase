cfc54da2b1b38bf0903cbf91827acb020004d30d
“[Zubair <[zubairbhuian@gmail.com]>
2022-03-19 11:02:02 +0600

    19.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:test_firebase/userform.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
-
-
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -18,8 +16,34 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.purple),
-      home:const UserForm(),
+      home: GetUserName( "documentId"),
     );
   }
 }
 
+class GetUserName extends StatelessWidget {
+  // const GetUserName({Key? key}) : super(key: key);
+  final String documentId;
+  GetUserName(this.documentId);
+
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+    return FutureBuilder<DocumentSnapshot>(future: users.doc(documentId).get(),builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot){
+           if (snapshot.hasError) {
+          return Text("Something went wrong");
+        }
+
+        if (snapshot.hasData && !snapshot.data!.exists) {
+          return Text("Document does not exist");
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
+          return Text("Full Name: ${data['full_name']} ${data['last_name']}");
+        }
+
+        return Text("loading");
+    },);
+  }
+}
