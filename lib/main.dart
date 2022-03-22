782d38f7a77832a37bd9d4428804393a5b4ef0ba
“[Zubair <[zubairbhuian@gmail.com]>
2022-03-22 21:43:06 +0600

    22.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:test_firebase/user.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,36 +16,82 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.purple),
-      home: const GetUserName(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('user info'),
+        ),
+        body: UserInformation(),
+      ),
     );
   }
 }
 
-class GetUserName extends StatefulWidget {
-  const GetUserName({Key? key}) : super(key: key);
+// class GetUserName extends StatefulWidget {
+//   const GetUserName({Key? key}) : super(key: key);
 
+//   @override
+//   State<GetUserName> createState() => _GetUserNameState();
+// }
+
+// class _GetUserNameState extends State<GetUserName> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: const Text('Hello there')),
+//       body: StreamBuilder(
+//         stream: FirebaseFirestore.instance.collection('usersname').snapshots(),
+//         builder: (_, snapshot) {
+//           if (!snapshot.hasData) {
+//             return const Center(
+//               child: Text('Loading'),
+//             );
+//           }else{
+//            return GridView.builder(
+//               scrollDirection: Axis.vertical,
+//               itemCount:snapshot.data.,
+//               gridDelegate: gridDelegate,
+//               itemBuilder: itemBuilder
+//               )
+//           }
+//         },
+//       ),
+//     );
+//   }
+// }
+class UserInformation extends StatefulWidget {
   @override
-  State<GetUserName> createState() => _GetUserNameState();
+  _UserInformationState createState() => _UserInformationState();
 }
 
-class _GetUserNameState extends State<GetUserName> {
+class _UserInformationState extends State<UserInformation> {
+  final Stream<QuerySnapshot> _usersStream =
+      FirebaseFirestore.instance.collection('usersname').snapshots();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Hello there')),
-      body: StreamBuilder<List<User>>(
-          stream: readUsers(),
-          builder: (context, e) {
-            if (e.hasError) {
-              final User = e.data!;
-            }else(){
-              
-            }
-          }),
+    return StreamBuilder<QuerySnapshot>(
+      stream: _usersStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading");
+        }
+
+        return ListView(
+          children: snapshot.data!.docs.map((DocumentSnapshot document) {
+            Map<String, dynamic> data =
+                document.data()! as Map<String, dynamic>;
+
+            return ListTile(
+              title: Text(data['email']),
+              subtitle: Text(data['name']),
+            );
+          }).toList(),
+        );
+      },
     );
-    return ListView();
-    Stream<List<User>> readUsers() =>
-        FirebaseFirestore.instance.collection('usersname').snapshots().map(
-            (event) => event.docs.map((e) => User.fromJson(e.data())).toList());
   }
 }
